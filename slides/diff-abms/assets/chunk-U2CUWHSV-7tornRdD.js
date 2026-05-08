7b62b7e847457cbbe4c9928d7991cae691934ed0
Arnau Quera-Bofarull <arnauq@protonmail.com>
2026-05-08 11:04:16 -0400

    gmu talk

---
+++ slides/diff-abms/assets/chunk-U2CUWHSV-7tornRdD.js
@@ -0,0 +1,1 @@
+import{i as e}from"./chunk-FQFHLQFH-hMJDuBrk.js";import{I as t}from"./chunk-GRVEB7DL-BW_TN3ER.js";var n=e((e,n)=>{let r;return n===`sandbox`&&(r=t(`#i`+e)),t(n===`sandbox`?r.nodes()[0].contentDocument.body:`body`).select(`[id="${e}"]`)},`getDiagramElement`);export{n as t};
